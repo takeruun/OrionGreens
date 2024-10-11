38df3ef1e1ae367444c7eb7a5cb1dd45849703fb
takeru.hashimoto <hstake25@gmail.com>
2024-10-11 19:31:53 +0900

    1人プランのあるコース検索スクリプト

---
+++ src/index.ts
@@ -0,0 +1,119 @@
+import 'dotenv/config';
+
+import axiosClient from '@aspida/axios';
+import { chromium, Locator } from 'playwright';
+
+import api from './api/rakuten.ts/$api';
+import { Methods } from './api/rakuten.ts/Gora/GoraGolfCourseSearch/20170623';
+
+const isRecordVideo = process.env.IS_RECORD_VIDEO === 'true';
+const isHeadless = process.env.IS_HEADLESS === 'true';
+
+const checkHavingSoloPlan = async (elements: Locator[]): Promise<boolean> => {
+  for (const element of elements) {
+    // 親要素を取得
+    const parent = element.locator('xpath=..');
+    if (!parent) {
+      continue;
+    }
+    const parentParent = parent.locator('xpath=..');
+    if (!parentParent) {
+      continue;
+    }
+    // <img>要素を取得
+    const imgs = await parentParent.locator('img').all();
+    if (imgs.length === 0) {
+      continue;
+    }
+
+    const isSoloPlan = imgs.every(async (img) => {
+      const src = await img.getAttribute('src');
+      return (
+        src ===
+        'https://image.gora.golf.rakuten.co.jp/img/avatar/60X60/none.png'
+      );
+    });
+    if (isSoloPlan) {
+      return true;
+    }
+  }
+  return false;
+};
+
+(async () => {
+  // Setup
+  const browser = await chromium.launch({
+    headless: isHeadless,
+    args: [
+      '--lang=ja',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+    ],
+  });
+  const context = await browser.newContext({
+    viewport: { width: 1920, height: 1080 },
+    recordVideo: isRecordVideo
+      ? {
+          dir: 'videos',
+        }
+      : undefined,
+  });
+  await context.route('**.jpg', (route) => route.abort());
+
+  const page = await context.newPage();
+
+  const client = api(axiosClient());
+  const params: Methods['get']['query'] = {
+    applicationId: process.env.RAKUTEN_APPLICATION_ID || '',
+    affiliateId: process.env.RAKUTEN_AFFILIATE_ID || '',
+    format: 'json',
+    formatVersion: '2',
+    hits: 2,
+  };
+
+  const res = await client.Gora.GoraGolfCourseSearch.$20170623.get({
+    query: params,
+  });
+
+  for (const item of res.body.Items || []) {
+    const url = item.golfCourseDetailUrl;
+    if (!url) {
+      continue;
+    }
+
+    await page.goto(url);
+
+    await page.getByRole('button', { name: '人予約' }).click();
+
+    await page.getByRole('button', { name: '検索する' }).click();
+    await page.waitForTimeout(2000);
+
+    const isDayChange = false;
+    if (isDayChange) {
+      await page.locator('#day').click();
+      await page.locator('#day').fill('20');
+      await page.getByRole('button', { name: '閉じる' }).click();
+      await page.waitForTimeout(2000);
+      await page.getByRole('button', { name: '検索する' }).click();
+    }
+
+    // 「あと1名で開催」テキストがない場合はスキップ
+    if ((await page.getByText('あと1名で開催').count()) === 0) {
+      continue;
+    }
+
+    // 「あと1名で開催」がある要素すべて取得
+    const elements = await page.getByText('あと1名で開催').all();
+    const isHavingSoloPlan = await checkHavingSoloPlan(elements);
+
+    if (isHavingSoloPlan) {
+      console.log('明日予約、1名開催プランを持っているコースです');
+      console.log(url);
+    }
+  }
+
+  await context.close();
+  await browser.close();
+})();
